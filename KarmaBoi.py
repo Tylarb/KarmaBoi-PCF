6c5e8935f58b26f0a7f8a987eba994919d6c2376
Tyler Ramer <tyaramer@gmail.com>
2017-10-09 23:13:22 -0400

    basic bot is ready :)

--- KarmaBoi.py
+++ KarmaBoi.py
@@ -2,13 +2,17 @@
 
 
 
-def karma_ask
+def karma_ask(name):
+    karma = 10
+    return karma
 
+def karma_add(name):
+    karma = karma_ask(name) + 1
+    return karma
 
-def karma_add
+def karma_sub(name):
+    karma = karma_ask(name) - 1
+    return karma
 
-def user_add
-
-
-
-
+def user_add(name):
+    return name
--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+import KarmaBoi
 
-print('hello world')
+print('This is a Karma Boi')
 
-print 'hello world'
+name = input('please provide a name:\n')
+print('You\'ve chosen ' + name)
+
+
+karma = KarmaBoi.karma_ask(name)
+karma = KarmaBoi.karma_sub(name)
+print(name + ' has ' + str(karma) + ' points of karma' )
